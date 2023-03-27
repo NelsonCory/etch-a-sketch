33806f5851bf62bfb5239d60f5b325b6989ab307
Cory <nelson.cory@outlook.com>
2023-03-27 01:40:00 -0400

    Grid Elements Change On-Hover

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let etchaSketch = {
     
 } 
 
-function add_element() {
+function initialize_grid() {
 
     for(let i = 0; i < etchaSketch.size; i++){
         const tileRow = document.createElement('div');
@@ -14,12 +14,38 @@ function add_element() {
         for(let j = 0;j < etchaSketch.size;j++){
             const template = document.createElement('div');
             template.classList.add('sketchTile');
+            template.classList.add('whiteTile');
+			template.addEventListener('mouseover', ()=> {
+				change_color(template);
+			});
             //template.innerHTML = etchaSketch.canvasArr[i];
             document.getElementById('tileRow'+i).appendChild(template);
             
         }
     }
-
 }
 
+function change_color(element){
+	
+	if(element.classList.contains('whiteTile')){
+		element.classList.remove('whiteTile');
+		element.classList.add('blackTile');
+	}
+	else if (element.classList.contains('blackTile')){
+		element.classList.remove('blackTile');
+		element.classList.add('whiteTile');
+	}
+	
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+	//this function runs when document has loaded
+	const btn = document.querySelector('#btn');
+	btn.addEventListener('click', () => {
+	  alert("Hello World");
+	});
+		
+	initialize_grid();
+});
 
